9c293596fdc691da3a30f744cbdd4a2295def8d1
Sumshi <sumayasomow@gmail.com>
2023-08-28 10:07:35 +0000

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)/*parent has left child already*/
 	{
 		new_node->left = parent->left;
-        parent->left->parent = new_node;
+		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
