2fa341d37c6130e928ab53c1ed1ed346887adb3d
Sumshi <sumayasomow@gmail.com>
2023-08-28 11:12:02 +0000

    deletes a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree:  pointer to the root node of the tree to delete
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	/*recursively deletes left, right nodes and root node*/
+	binary_tree_delete(tree->left);/*deletes left subtree*/
+	binary_tree_delete(tree->right);/*deletes right sub tree*/
+	free(tree);/*deletes current node*/
+}
