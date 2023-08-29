a01df2d4937f7cd96c70be4effa5c35e9bdf0bb9
Sumshi <sumayasomow@gmail.com>
2023-08-29 11:33:32 +0000

    finds siblings of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,25 +7,24 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
 
 	if (node == NULL)
 	{
 		return (NULL);
 	}
-	if (parent == NULL)
+	if (node->parent == NULL)
 	{
 		return (NULL);
 	}
 	/*check if node is left child*/
-	if (parent->left == node)
+	if (node->parent->left == node)
 	{
-		return (parent->right);/*returns the right child*/
+		return (node->parent->right);/*returns the right child*/
 	}
 	/*check if node is right child*/
-	else if (parent->right == node)
+	else if (node->parent->right == node)
 	{
-		return (parent->left);/*returns the left child*/
+		return (node->parent->left);/*returns the left child*/
 	}
 	else/*no sibling for either left or right*/
 	{
