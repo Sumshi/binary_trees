30dfc9337121352355c36e52576da961fc90b54b
Sumshi <sumayasomow@gmail.com>
2023-08-29 07:33:12 +0000

    size of binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,18 +8,18 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {/*size of a tree = number of elements present in the tree*/
 
+	size_t left_size;
+	size_t right_size;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
 	/*get the size of left subtree*/
-	size_t left_size = binary_tree_size(tree->left);
+	left_size = binary_tree_size(tree->left);
 
 	/*get the size of right subtree*/
-	size_t right_size = binary_tree_size(tree->right);
-	size_t  tree_size;
-
-	tree_size = left_size + right_size + 1;/*for the current node*/
-	return (tree_size);
+	right_size = binary_tree_size(tree->right);
+	return (left_size + right_size + 1);/*1 for the current node*/
 }
